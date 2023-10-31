7831f13af3c4f631d134b36b4a6a82f6c915a31f
bentomsett <ben@tomsett.xyz>
2023-10-31 15:27:34 +0000

    Add training audio (and recording script)

---
+++ feature_extraction.py
@@ -0,0 +1,3 @@
+# feature_extraction.py
+# Extracts features from audio files for use in training/testing/using a neural network for speech recognition
+# Author: Ben Tomsett
---
+++ speech_recorder.py
@@ -0,0 +1,63 @@
+# speech_recorder.py
+# Reads the list in names.txt, and records 20 2-second clips of each name in the list
+# Author: Ben Tomsett
+
+import os
+import sounddevice as sd
+import soundfile as sf
+
+os.system("clear")
+
+file = open("names.txt", "r")
+names = file.read().split("\n")
+file.close()
+
+print("Loaded " + str(len(names)) + " names.")
+input("Press enter to start...")
+
+# Recording constants
+sample_rate = 16000
+clip_length = 2
+
+for name in names:
+    i = 0
+
+    while i < 20:
+        os.system("clear")
+
+        print("Name: " + name)
+        print("Recording " + str(i + 1) + " of 20")
+
+        input("Press enter to start recording...")
+
+        audio = sd.rec(clip_length * sample_rate, channels=1, samplerate=sample_rate)
+        sd.wait()
+
+        print("Recording finished, playing back audio...")
+
+        sd.play(audio, sample_rate)
+        sd.wait()
+
+        save = input("Save audio? (y/n): ").lower()
+
+        if save == "y":
+            print("Recording finished, saving .wav file...")
+            sf.write(f'./audio/training/{name}-{i + 1}.wav', audio, sample_rate)
+            print("Saved .wav file.")
+
+            i += 1
+        else:
+            print("Recording discarded.")
+
+# Record audio and wait until finished recording
+# print("Starting recording...")
+# r = sd.rec(seconds * fs, channels=1, samplerate=fs)
+# sd.wait()
+#
+# print("Recording finished, plotting waveform and starting playback")
+#
+# print("Saving .wav file...")
+# sf.write(f'speech{round(time.time() * 1000)}.wav', r, fs)
+
+
+
